7810058188918877b5fca8e0854a404848fc8d68
snowlight-aemt <it.s.aemt@gmail.com>
2023-03-12 17:44:23 +0900

    Create Servlet & Register Servlet

--- src/main/java/me/snowlight/helloboot/HellobootApplication.java
+++ src/main/java/me/snowlight/helloboot/HellobootApplication.java
@@ -2,13 +2,30 @@ package me.snowlight.helloboot;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class HellobootApplication {
 
 	public static void main(String[] args) {
 		ServletWebServerFactory servletWebServerFactory = new TomcatServletWebServerFactory();
-		WebServer webServer = servletWebServerFactory.getWebServer();
+		WebServer webServer = servletWebServerFactory.getWebServer(servletContext -> {
+			servletContext.addServlet("hello", new HttpServlet() {
+				@Override
+				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+					resp.setStatus(200);
+					resp.setHeader("Content-Type", "text/plain");
+					resp.getWriter().println("Hello Servlet");
+				}
+			}).addMapping("/hello");
+		});
+
 		webServer.start();
 	}
 
