cf8da5dd981abb0bdcef29ca4f75efe44382a1a4
snowlight-aemt <it.s.aemt@gmail.com>
2023-03-12 17:23:18 +0900

    Start Tomcat Servlet Web Server - Servlet Container

--- src/main/java/me/snowlight/helloboot/HellobootApplication.java
+++ src/main/java/me/snowlight/helloboot/HellobootApplication.java
@@ -1,13 +1,15 @@
 package me.snowlight.helloboot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 
-@SpringBootApplication
 public class HellobootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HellobootApplication.class, args);
+		ServletWebServerFactory servletWebServerFactory = new TomcatServletWebServerFactory();
+		WebServer webServer = servletWebServerFactory.getWebServer();
+		webServer.start();
 	}
 
 }
