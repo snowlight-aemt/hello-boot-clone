1a2708fbb7cb8281a0fa227072ad669bd33fb181
snowlight-aemt <it.s.aemt@gmail.com>
2023-03-13 21:36:16 +0900

    하나의 front controller (servlet) 안에서 uri & http method 구분

--- src/main/java/me/snowlight/helloboot/HellobootApplication.java
+++ src/main/java/me/snowlight/helloboot/HellobootApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServlet;
@@ -16,16 +20,21 @@ public class HellobootApplication {
 	public static void main(String[] args) {
 		ServletWebServerFactory servletWebServerFactory = new TomcatServletWebServerFactory();
 		WebServer webServer = servletWebServerFactory.getWebServer(servletContext -> {
-			servletContext.addServlet("hello", new HttpServlet() {
+			servletContext.addServlet("frontcontroller", new HttpServlet() {
 				@Override
 				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-					String name = req.getParameter("name");
+					String uri = req.getRequestURI();
+					if (uri.equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+						String name = req.getParameter("name");
 
-					resp.setStatus(200);
-					resp.setHeader("Content-Type", "text/plain");
-					resp.getWriter().println("Hello Servlet " + name);
+						resp.setStatus(HttpStatus.OK.value());
+						resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+						resp.getWriter().println("Hello Servlet " + name);
+					} else {
+						resp.setStatus(HttpStatus.NOT_FOUND.value());
+					}
 				}
-			}).addMapping("/hello");
+			}).addMapping("/*");
 		});
 
 		webServer.start();
