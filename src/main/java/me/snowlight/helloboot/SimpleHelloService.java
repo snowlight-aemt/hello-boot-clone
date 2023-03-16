b8c809fce1451d5e98485082dc207c2c8c62708e
snowlight-aemt <it.s.aemt@gmail.com>
2023-03-16 22:09:51 +0900

    IoC 의 DI 추가

---
+++ src/main/java/me/snowlight/helloboot/SimpleHelloService.java
@@ -0,0 +1,9 @@
+package me.snowlight.helloboot;
+
+public class SimpleHelloService  implements HelloService {
+
+    @Override
+    public String sayHello(String name) {
+        return "hello " + name;
+    }
+}
