d9d5d7874e87c874cc402c3edf4b4fa9ea8ba70e
snowlight-aemt <it.s.aemt@gmail.com>
2023-05-18 22:11:37 +0900

    Servlet Containner 를 실행을 실행 시점에 실행 될 수 있게 수정

--- src/main/java/me/snowlight/helloboot/HellobootApplication.java
+++ src/main/java/me/snowlight/helloboot/HellobootApplication.java
@@ -21,20 +21,24 @@ import java.io.IOException;
 public class HellobootApplication {
 
 	public static void main(String[] args) {
-		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
+		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+			@Override
+			protected void onRefresh() {
+				ServletWebServerFactory servletWebServerFactory = new TomcatServletWebServerFactory();
+				WebServer webServer = servletWebServerFactory.getWebServer(servletContext -> {
+
+					servletContext.addServlet("dispatcherServlet",
+									new DispatcherServlet(this))
+							.addMapping("/*");
+				});
+
+				webServer.start();
+			}
+		};
 		applicationContext.registerBean(HelloController.class);
 		applicationContext.registerBean(SimpleHelloService.class);
 		applicationContext.refresh();
 
-		ServletWebServerFactory servletWebServerFactory = new TomcatServletWebServerFactory();
-		WebServer webServer = servletWebServerFactory.getWebServer(servletContext -> {
-
-			servletContext.addServlet("dispatcherServlet",
-							new DispatcherServlet(applicationContext))
-					.addMapping("/*");
-		});
-
-		webServer.start();
  	}
 
 }
