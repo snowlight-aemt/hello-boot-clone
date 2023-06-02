f66280cc066a2c3ca4097016f1ac8e62c716f00a
snowlight-aemt <it.s.aemt@gmail.com>
2023-06-02 21:26:30 +0900

    ImportCandidates 를 사용하여 특정 파일의 클래스 정보를 세팅

--- src/main/java/me/snowlight/config/MyAutoConfigImportSelector.java
+++ src/main/java/me/snowlight/config/MyAutoConfigImportSelector.java
@@ -1,14 +1,21 @@
 package me.snowlight.config;
 
+import org.springframework.boot.context.annotation.ImportCandidates;
 import org.springframework.context.annotation.DeferredImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 
+import java.util.stream.StreamSupport;
+
 public class MyAutoConfigImportSelector implements DeferredImportSelector {
+    private final ClassLoader classLoader;
+
+    public MyAutoConfigImportSelector(ClassLoader classLoader) {
+        this.classLoader = classLoader;
+    }
+
     @Override
     public String[] selectImports(AnnotationMetadata importingClassMetadata) {
-        return new String[] {
-            "me.snowlight.config.authconfig.DispatcherServletConfig",
-            "me.snowlight.config.authconfig.TomcatWebServerConfig"
-        };
+        ImportCandidates candidates = ImportCandidates.load(MyAutoConfiguration.class, this.classLoader);
+        return StreamSupport.stream(candidates.spliterator(), false).toArray(String[]::new);
     }
 }
