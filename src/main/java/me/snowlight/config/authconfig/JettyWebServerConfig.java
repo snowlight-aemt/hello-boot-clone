5cf69161e9dc49e7d1582a8930825913cd4ab5ae
snowlight-aemt <it.s.aemt@gmail.com>
2023-06-03 15:14:08 +0900

    @Conditional 선택 기준을 라이브러리 존재 유무  (Jetty)

--- src/main/java/me/snowlight/config/authconfig/JettyWebServerConfig.java
+++ src/main/java/me/snowlight/config/authconfig/JettyWebServerConfig.java
@@ -1,5 +1,6 @@
 package me.snowlight.config.authconfig;
 
+import com.fasterxml.jackson.databind.util.ClassUtil;
 import me.snowlight.config.MyAutoConfiguration;
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
@@ -8,6 +9,7 @@ import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.core.type.AnnotatedTypeMetadata;
+import org.springframework.util.ClassUtils;
 
 @MyAutoConfiguration
 @Conditional(JettyWebServerConfig.JettyCondition.class)
@@ -20,7 +22,7 @@ public class JettyWebServerConfig {
     static class JettyCondition implements Condition {
         @Override
         public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-            return true;
+            return ClassUtils.isPresent("org.eclipse.jetty.server.Server", context.getClassLoader());
         }
     }
 }
