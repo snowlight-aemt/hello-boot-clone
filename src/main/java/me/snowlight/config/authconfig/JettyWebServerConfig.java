3ba0090760f44a4eac3047692838be54975b454e
snowlight-aemt <it.s.aemt@gmail.com>
2023-06-03 15:31:53 +0900

    @Conditional 리팩토링 - Custom Annotation 를 구현 - 중복 Condition Class

--- src/main/java/me/snowlight/config/authconfig/JettyWebServerConfig.java
+++ src/main/java/me/snowlight/config/authconfig/JettyWebServerConfig.java
@@ -1,28 +1,16 @@
 package me.snowlight.config.authconfig;
 
-import com.fasterxml.jackson.databind.util.ClassUtil;
+import me.snowlight.config.ConditionalMyOnClass;
 import me.snowlight.config.MyAutoConfiguration;
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Condition;
-import org.springframework.context.annotation.ConditionContext;
-import org.springframework.context.annotation.Conditional;
-import org.springframework.core.type.AnnotatedTypeMetadata;
-import org.springframework.util.ClassUtils;
 
 @MyAutoConfiguration
-@Conditional(JettyWebServerConfig.JettyCondition.class)
+@ConditionalMyOnClass("org.eclipse.jetty.server.Server")
 public class JettyWebServerConfig {
     @Bean
     public ServletWebServerFactory servletWebServerFactory() {
         return new JettyServletWebServerFactory();
-    }
-
-    static class JettyCondition implements Condition {
-        @Override
-        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-            return ClassUtils.isPresent("org.eclipse.jetty.server.Server", context.getClassLoader());
-        }
     }
 }
