bdca19e7ba471c4951819576bc70b2b6edb673bd
snowlight-aemt <it.s.aemt@gmail.com>
2023-07-27 22:51:52 +0900

    JdbcTemplate과 트랜잭션 매니저 구성

--- src/main/java/me/snowlight/config/authconfig/MyDataSourceConfig.java
+++ src/main/java/me/snowlight/config/authconfig/MyDataSourceConfig.java
@@ -5,8 +5,12 @@ import me.snowlight.config.ConditionalMyOnClass;
 import me.snowlight.config.EnableMyConfigurationProperties;
 import me.snowlight.config.MyAutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnSingleCandidate;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.jdbc.support.JdbcTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 import java.sql.Driver;
@@ -14,7 +18,8 @@ import java.sql.Driver;
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.springframework.jdbc.core.JdbcOperations")
 @EnableMyConfigurationProperties(MyDataSourceProperties.class)
-public class MyDataSourceConfig {
+@EnableTransactionManagement
+public class MyDataSourceCon fig {
     @Bean
     @ConditionalMyOnClass("com.zaxxer.hikari.HikariDataSource")
     @ConditionalOnMissingBean
@@ -38,5 +43,19 @@ public class MyDataSourceConfig {
         dataSource.setPassword(properties.getPassword());
 
         return dataSource;
+    }
+
+    @Bean
+    @ConditionalOnSingleCandidate(DataSource.class)
+    @ConditionalOnMissingBean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+
+    @Bean
+    @ConditionalOnSingleCandidate(DataSource.class)
+    @ConditionalOnMissingBean
+    public JdbcTransactionManager transactionManager(DataSource dataSource) {
+        return new JdbcTransactionManager(dataSource);
     }
 }
---
+++ src/test/java/me/snowlight/helloboot/JdbcTemplateTest.java
@@ -0,0 +1,36 @@
+package me.snowlight.helloboot;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@HellobootTest
+public class JdbcTemplateTest {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @BeforeEach
+    void init() {
+        jdbcTemplate.execute("create table if not exists hello(name varchar(50) primary key, count int)");
+    }
+
+    @Test
+    void InsertAndQuery() {
+        jdbcTemplate.update("insert into hello values(?, ?)", "Name-01", 3);
+        jdbcTemplate.update("insert into hello values(?, ?)", "Name-02", 5);
+
+        Long count = jdbcTemplate.queryForObject("select count(*) from hello", Long.class);
+        Assertions.assertThat(count).isEqualTo(2L);
+    }
+
+    @Test
+    void InsertAndQuery2() {
+        jdbcTemplate.update("insert into hello values(?, ?)", "Name-01", 3);
+        jdbcTemplate.update("insert into hello values(?, ?)", "Name-02", 5);
+
+        Long count = jdbcTemplate.queryForObject("select count(*) from hello", Long.class);
+        Assertions.assertThat(count).isEqualTo(2L);
+    }
+}
